34464f4a8b3cbc9fc61b757a1a6daf0155c457e6
Jessica Scianimanica <sciani.jessica@gmail.com>
2017-12-08 17:21:51 +0000

    added cloudinary

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem 'pg'
 gem 'puma'
 gem 'rails', '5.0.6'
 gem 'redis'
+gem 'devise'
+gem 'figaro'
+
 
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/quisontelles', to: 'pages#qui'
+  get '/prestations', to: 'pages#prestations'
+  get '/portfolio', to: 'pages#portfolio'
+  get '/studio', to: 'pages#studio'
+  get '/contact', to: 'pages#contact'
 end
