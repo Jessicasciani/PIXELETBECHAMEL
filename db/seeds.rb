6d65d30bdc6c3fa7f048926b945f1de5972f4de0
Jessica Scianimanica <sciani.jessica@gmail.com>
2018-09-12 10:43:26 +0100

    contact page andnavbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+["https://cdn.pixabay.com/photo/2016/06/18/17/42/image-1465348_960_720.jpg","https://cdn.pixabay.com/photo/2016/06/18/17/42/image-1465348_960_720.jpg","https://cdn.pixabay.com/photo/2016/06/18/17/42/image-1465348_960_720.jpg"].each do |link|
+  PhotoHome.create!(remote_photo_url: link)
+end
+
+["https://cdn.pixabay.com/photo/2016/06/18/17/42/image-1465348_960_720.jpg","https://cdn.pixabay.com/photo/2016/06/18/17/42/image-1465348_960_720.jpg","https://cdn.pixabay.com/photo/2016/06/18/17/42/image-1465348_960_720.jpg"].each do |link|
+  Folder.create!(client: "nom", remote_photo_url: link)
+end
+
+["https://cdn.pixabay.com/photo/2016/06/18/17/42/image-1465348_960_720.jpg","https://cdn.pixabay.com/photo/2016/06/18/17/42/image-1465348_960_720.jpg","https://cdn.pixabay.com/photo/2016/06/18/17/42/image-1465348_960_720.jpg"].each do |link|
+  VideoFolder.create!(client: "nom", remote_photo_url: link)
+end
